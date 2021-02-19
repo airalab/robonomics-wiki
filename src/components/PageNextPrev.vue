1987a1177d091ceb2d48a8f2e143d1b2a6555b0e
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2021-02-19 15:55:09 +0400

    sidebar

---
+++ src/components/PageNextPrev.vue
@@ -0,0 +1,62 @@
+<template>
+
+    <section class="pageNextPrev">
+
+        <a v-if="pagePrev" class="button button__primary" :href="pagePrev.link">
+            &larr; {{ pagePrev.title }}
+        </a>
+
+        <a v-if="pageNext" class="button button__primary" :href="pageNext.link">
+            {{ pageNext.title }} &rarr;
+        </a>
+
+    </section>
+
+</template>
+
+<style lang="scss" scoped>
+
+    .pageNextPrev {
+        padding: calc(var(--space)/2) 0;
+
+        display: grid;
+        gap: calc(var(--space)/2);
+
+        @media screen and (min-width:500px){
+          grid-template-columns: 1fr 1fr;
+        }
+
+        text-transform: uppercase
+    }
+
+</style>
+
+
+<script>
+
+export default {
+  props: {
+    current: null,
+    itemsList: null
+  },
+
+  computed: {
+      pagePrev() {
+        return this.itemsList[this.current - 1];
+      },
+
+      pageNext() {
+        return this.itemsList[this.current + 1];
+      },
+
+    //   pageCurrent() {
+    //     return Object.values(this.itemsList).findIndex(function(item) {
+    //         if(item.isOpen == true)
+    //         return true;
+    //     })
+    //   }
+  },
+
+}
+
+</script>
--- src/components/SidebarDocs.vue
+++ src/components/SidebarDocs.vue
@@ -1,13 +1,16 @@
 <template>
   <ul class="menu menu-tree">
-    <li v-for="(item, key) in list" :key="key" v-if="item.published!=false">
-      <!-- <a class="menu__item menu__link" v-if="item.link" :href="link(item.link)" :exact="item.link == '/docs/'">{{item.title}}</a> -->
-      <g-link class="menu__item menu__link" v-if="item.link" :to="item.link" :exact="item.link == '/docs/'">{{item.title}}</g-link>
+    <li v-for="(item, key) in list" :key="key">
+      <!-- <a class="menu__item menu__link" v-reload v-if="item.link && item.published!=false" :href="link(item.link)">{{item.title}}</a> -->
+      <g-link class="menu__item menu__link" v-if="item.link && item.published!=false" :to="item.link" :exact="item.link == '/docs/'">{{item.title}}</g-link>
       <template v-else>
-        <h4 class="menu__item menu__title" @click="toggle(key)">
+        <h4 class="menu__item menu__title" @click="toggle(key)" v-if="item.published!=false">
           {{item.title}}
-          <span>[{{ item.isOpen === true ? '-' : '+' }}]</span>
+          <span>[{{ item.isOpen === "true" ? '-' : '+' }}]</span>
         </h4>
+
+        <!-- {{ test(item.items) }} -->
+
         <List :items="item.items" v-show="item.isOpen"/>
       </template>
     </li>
@@ -22,9 +25,11 @@ import Vue from 'vue'
 
 export default {
   props: {
+    allList: null,
     items: {
       default: []
-    }
+    },
+    current: null
   },
   components: {
     List: () => import("./SidebarDocs.vue")
@@ -39,7 +44,27 @@ export default {
     },
     link (path){
       return window.location.protocol + "//" + window.location.host + path; 
-    }
-  }
+    },
+    // test(o) {
+    //   return o.some(i => i.link === this.current)
+    // }
+  },
+
+
+  // directives: {
+  //     reload: {
+  //       inserted: function (el, binding, vnode) {
+  //         el.addEventListener('click', (evt) => {
+  //           // the URL we want to go to will be in el.href for g-link, a, b-button.
+  //           // it will be in evt.target.href for b-nav-item and b-navbar-brand (because those are composite components
+  //           // that render as <li><a></li>, or something like that)
+  //           if (process.isClient) {
+  //             evt.preventDefault();
+  //             window.location.href = evt.target.href || el.href;
+  //           }
+  //         });
+  //       }
+  //     }
+  //   },
 }
 </script>
