09cabee0001b01e56e9134f67652b7bc5d79bb34
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2021-04-12 23:32:59 +0400

    test route

--- src/main.js
+++ src/main.js
@@ -99,15 +99,11 @@ export default function (Vue, { router, head, isClient, appOptions }) {
         console.log('enterpath === to.path')
         return next()
       }
-      else {
-        return next({
-          path: enterpath
-        })
-      }
 
-      // return next({
-      //   path: enterpath
-      // })
+      return next({
+        path: enterpath,
+        replace: true
+      })
 
     })
   }
