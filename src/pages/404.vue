2d4131f2811413f22586bcecaebe5f49c64fa060
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2021-04-13 11:46:18 +0400

    test

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
-
-    <h1>{{$st('404msg', $store.state.locale)}}</h1>
-
+    <div>
+      <h1>{{ $st("404msg", $store.state.locale) }}</h1>
+    </div>
   </Layout>
 </template>
