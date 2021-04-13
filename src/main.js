8d257fb36d40bc99e8ef164241fbf04981d53f64
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2021-04-13 08:57:29 +0400

    test

--- src/main.js
+++ src/main.js
@@ -87,10 +87,18 @@ export default function (Vue, { router, head, isClient, appOptions }) {
 
 
   //Rewrite route according to locale
-  if (isClient) {
+  if (process.isClient) {
     router.beforeEach((to, from, next) => {
 
       const enterpath = translatePath(to.path || '/', appOptions.store.state.locale)
+
+      // do not rewrite build paths
+
+      if (process.isServer) {
+
+        return next()
+
+      }
 
       // console.log('to.path ' + to.path)
       // console.log('enterpath ' + enterpath)
