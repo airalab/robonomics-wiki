558859117c2751ff86186e39566b1e8390fd47c3
Anastasiia Bakai <anastasiia.bakai@gmail.com>
2021-08-26 20:29:39 +0400

    asciinema video fix preview

--- src/components/Asciinema.vue
+++ src/components/Asciinema.vue
@@ -1,5 +1,10 @@
 <template>
-    <script type="application/javascript" :src="'https://asciinema.org/a/'+vid+'.js'" :id="'asciicast-' + vid" async :data-autoplay="autoplay"></script>
+    <!-- <script type="application/javascript" :src="'https://asciinema.org/a/'+vid+'.js'" :id="'asciicast-' + vid" async :data-autoplay="autoplay"></script> -->
+
+    <p><g-link :href="'https://asciinema.org/a/'+vid">
+      <img :src="'https://asciinema.org/a/'+ vid + '.png'"/>
+      <span>Click on picture to view asciinema video</span>
+    </g-link></p>
 </template>
 
 <script>
