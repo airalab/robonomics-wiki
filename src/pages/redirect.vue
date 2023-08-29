7858a0888bccb11126f3ab6ce9531dfa9b54848a
Julia <uug11@mail.ru>
2023-08-29 16:51:03 +0400

    removed robonomics-coffee + did redirect

---
+++ src/pages/redirect.vue
@@ -0,0 +1,48 @@
+<template>
+    <layout>
+        <div class="redirect"> <span>This page has moved to a</span> <a :href="redirect">{{ redirect }}</a> <span>Automatic redirecting...</span> </div>
+    </layout>
+</template>
+
+<script>
+export default {
+  metaInfo() {
+        return {
+            meta: [
+                {
+                    'http-equiv': 'refresh',
+                    content: `0; url="${this.redirect}"`
+                }
+            ],
+
+            link: () => [
+                {
+                    rel: 'canonical',
+                    href: `${this.redirect}`,
+                    id: 'canonical',
+                },
+            ],
+        }
+  },
+
+  computed: {
+      redirect(){
+          if(this.$context.redirect){
+              return this.$context.redirect
+          }
+      }
+  }
+}
+</script>
+
+<style scoped>
+    .redirect {
+        min-height: 100vh;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        font-weight: 600;
+    }
+</style>
